c428097d8b2ea82bb86d5af785059af666ec8b45
Imngzx <zhixuanng14@gmail.com>
2026-03-07 23:12:19 +0800

    added support for neovide

--- init.lua
+++ init.lua
@@ -60,26 +60,5 @@ require('libs.lazy').trigger_verylazy()
 --lsp loading
 require('custom.lsp-loading')
 
-if vim.g.neovide then
-  vim.o.guifont =
-  'JetBrainsMono Nerd Font:h14' -- Replace h14 with your desired font size remove :b for regular font
-  vim.g.neovide_window_blurred = true
-  -- vim.g.neovide_opacity = 0.93
-  vim.g.neovide_floating_blur_amount_x = 3.0
-  vim.g.neovide_floating_blur_amount_y = 3.0
-  vim.g.neovide_refresh_rate = 75
-  vim.g.neovide_cursor_antialiasing = true
-  vim.g.neovide_hide_titlebar = true
-  vim.g.neovide_padding_bottom = -2
-  vim.g.neovide_floating_shadow = false
-  vim.g.neovide_window_blurred = true
-  vim.g.neovide_opacity = 0.8
-  vim.g.neovide_normal_opacity = 0.8
-  if vim.g.neovide then
-    -- Running in Neovide → disable snacks scroll
-    vim.g.snacks_scroll = false
-  else
-    -- Running in terminal → enable snacks scroll
-    vim.g.snacks_scroll = true
-  end
-end
+--neovide
+require('config.neovide')
---
+++ lua/config/neovide.lua
@@ -0,0 +1,22 @@
+if vim.g.neovide then
+  vim.opt.guifont = { 'JetBrainsMono Nerd Font', ':h14' }
+  -- vim.g.neovide_window_blurred = true
+  -- vim.g.neovide_opacity = 0.93
+  vim.g.neovide_floating_blur_amount_x = 3.0
+  vim.g.neovide_floating_blur_amount_y = 3.0
+  vim.g.neovide_refresh_rate = 240
+  vim.g.neovide_cursor_antialiasing = true
+  vim.g.neovide_hide_titlebar = true
+  -- vim.g.neovide_padding_bottom = -2
+  vim.g.neovide_floating_shadow = false
+  vim.g.neovide_window_blurred = true
+  -- vim.g.neovide_opacity = 0.8
+  -- vim.g.neovide_normal_opacity = 0.8
+  if vim.g.neovide then
+    -- Running in Neovide → disable snacks scroll
+    vim.g.snacks_scroll = false
+  else
+    -- Running in terminal → enable snacks scroll
+    vim.g.snacks_scroll = true
+  end
+end
