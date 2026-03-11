b684b8f36aa6427f93949b96fecc40b841bb7e53
Imngzx <zhixuanng14@gmail.com>
2026-03-11 20:16:52 +0800

    changed how im-select lazyloaded and behave

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -5,7 +5,7 @@ local lazy = require('libs.lazy')
 lazy.load({
   plugin = 'https://github.com/keaising/im-select.nvim',
   -- 当打开已存在文件或新建文件时加载
-  event = { 'BufReadPost', 'BufNewFile' },
+  event = { 'InsertEnter' },
   -- 如果你想极致一点，可以改成 event = 'InsertEnter'，只有你按下 i 准备打字时才加载
   setup = function()
     require('im_select').setup({
@@ -13,7 +13,7 @@ lazy.load({
       set_default_events = { 'VimEnter', 'FocusGained', 'InsertLeave', 'CmdlineLeave' },
       set_previous_events = { 'InsertEnter' },
       keep_quiet_on_no_binary = false,
-      async_switch_im = false,
+      async_switch_im = true,
     })
   end
 })
