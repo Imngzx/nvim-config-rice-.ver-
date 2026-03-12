75ef2eb386a12befa25d3e548026a1a45d18401a
Imngzx <zhixuanng14@gmail.com>
2026-03-12 19:27:18 +0800

    made tab shows even only 1 tab opening

--- init.lua
+++ init.lua
@@ -31,7 +31,7 @@ require('custom.incline').setup()
 require('custom.transparent').setup({ auto_enable = false })
 local icons = require('libs.icons')
 require('custom.tabline').setup({
-  hide_single_tab = true,
+  hide_single_tab = false,
   on_close = function(buf_id) Snacks.bufdelete(buf_id) end,
   file_icons = function(name) return Snacks.util.icon(name, 'file') end,
   icons = { close = icons.basic.close, modify = icons.basic.modify }
