68125a004e81faf00b0aa8eb0049f4218b9803a3
Imngzx <zhixuanng14@gmail.com>
2026-03-13 11:00:08 +0800

    removed some useless comments (commented useless plugins)

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,21 +1,5 @@
 local lazy = require('libs.lazy')
 
--- [Cursor]
--- lazy.load({
---   plugin = 'https://github.com/sphamba/smear-cursor.nvim',
---   event = { 'User', pattern = 'VeryLazy' },
---   setup = function()
---     require('smear_cursor').setup({
---       smear_between_buffers = true,
---     })
---   end
--- })
--- run after 100ms
-
--- [Icon]
--- vim.pack.add({ 'https://github.com/nvim-mini/mini.icons' })
--- require('mini.icons').setup()
--- require('mini.icons').mock_nvim_web_devicons()
 lazy.load({
   plugin = 'https://github.com/nvim-mini/mini.icons',
   -- UI 渲染完毕后，在后台静默加载
