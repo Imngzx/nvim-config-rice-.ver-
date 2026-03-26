b7be27c52b84e7e2319aa75c900aba704ac5bad2
Imngzx <zhixuanng14@gmail.com>
2026-03-26 20:53:49 +0800

    added a friendly guide for coloring

---
+++ note/color-list.lua
@@ -0,0 +1,247 @@
+-- ========================================================
+-- 🎨 48-Color Palette for Ricing
+-- ========================================================
+
+local ricer_palette = {
+  abyssal_void = {
+    hex = '#0B090A',
+    rgb = 'rgb(11, 9, 10)',
+    hsl = 'hsl(330, 10%, 4%)'
+  },
+  terminal_ash = {
+    hex = '#2B2D42',
+    rgb = 'rgb(43, 45, 66)',
+    hsl = 'hsl(235, 21%, 21%)'
+  },
+  mech_armor = {
+    hex = '#4A4E69',
+    rgb = 'rgb(74, 78, 105)',
+    hsl = 'hsl(232, 17%, 35%)'
+  },
+  ghost_shell = {
+    hex = '#F8F9FA',
+    rgb = 'rgb(248, 249, 250)',
+    hsl = 'hsl(210, 20%, 98%)'
+  },
+  cloud_sync = {
+    hex = '#E0E1DD',
+    rgb = 'rgb(224, 225, 221)',
+    hsl = 'hsl(75, 5%, 87%)'
+  },
+  aluminium = {
+    hex = '#ADB5BD',
+    rgb = 'rgb(173, 181, 189)',
+    hsl = 'hsl(210, 11%, 71%)'
+  },
+  cyber_sangria = {
+    hex = '#E63946',
+    rgb = 'rgb(230, 57, 70)',
+    hsl = 'hsl(355, 78%, 56%)'
+  },
+  glitch_magenta = {
+    hex = '#F72585',
+    rgb = 'rgb(247, 37, 133)',
+    hsl = 'hsl(333, 93%, 56%)'
+  },
+  neon_bubblegum = {
+    hex = '#FF71CE',
+    rgb = 'rgb(255, 113, 206)',
+    hsl = 'hsl(321, 100%, 72%)'
+  },
+  radioactive_tag = {
+    hex = '#FF9F1C',
+    rgb = 'rgb(255, 159, 28)',
+    hsl = 'hsl(35, 100%, 55%)'
+  },
+  retro_apricot = {
+    hex = '#F4A261',
+    rgb = 'rgb(244, 162, 97)',
+    hsl = 'hsl(27, 87%, 67%)'
+  },
+  rusty_board = {
+    hex = '#CB997E',
+    rgb = 'rgb(203, 153, 126)',
+    hsl = 'hsl(21, 42%, 65%)'
+  },
+  voltage_yellow = {
+    hex = '#FEE440',
+    rgb = 'rgb(254, 228, 64)',
+    hsl = 'hsl(52, 99%, 62%)'
+  },
+  cyber_mustard = {
+    hex = '#E9C46A',
+    rgb = 'rgb(233, 196, 106)',
+    hsl = 'hsl(43, 74%, 66%)'
+  },
+  matrix_phosphor = {
+    hex = '#00FF41',
+    rgb = 'rgb(0, 255, 65)',
+    hsl = 'hsl(135, 100%, 50%)'
+  },
+  toxic_matcha = {
+    hex = '#84DCC6',
+    rgb = 'rgb(132, 220, 198)',
+    hsl = 'hsl(165, 56%, 69%)'
+  },
+  electric_cyan = {
+    hex = '#0DF5E3',
+    rgb = 'rgb(13, 245, 227)',
+    hsl = 'hsl(175, 92%, 51%)'
+  },
+  neon_ice = {
+    hex = '#00BBF9',
+    rgb = 'rgb(0, 187, 249)',
+    hsl = 'hsl(195, 100%, 49%)'
+  },
+  deep_sea_cable = {
+    hex = '#0077B6',
+    rgb = 'rgb(0, 119, 182)',
+    hsl = 'hsl(201, 100%, 36%)'
+  },
+  kernel_panic = {
+    hex = '#1D3557',
+    rgb = 'rgb(29, 53, 87)',
+    hsl = 'hsl(215, 50%, 23%)'
+  },
+  void_purple = {
+    hex = '#3A0CA3',
+    rgb = 'rgb(58, 12, 163)',
+    hsl = 'hsl(258, 86%, 34%)'
+  },
+  neon_eva = {
+    hex = '#B10DC9',
+    rgb = 'rgb(177, 13, 201)',
+    hsl = 'hsl(292, 88%, 42%)'
+  },
+  lavender_mem = {
+    hex = '#CDB4DB',
+    rgb = 'rgb(205, 180, 219)',
+    hsl = 'hsl(278, 25%, 77%)'
+  },
+  hologram_violet = {
+    hex = '#9D4EDD',
+    rgb = 'rgb(157, 78, 221)',
+    hsl = 'hsl(273, 67%, 59%)'
+  },
+  -- [NEW COLORS START]
+  plasma_burn = {
+    hex = '#6A00F4',
+    rgb = 'rgb(106, 0, 244)',
+    hsl = 'hsl(266, 100%, 48%)'
+  },
+  neural_static = {
+    hex = '#415A77',
+    rgb = 'rgb(65, 90, 119)',
+    hsl = 'hsl(212, 29%, 36%)'
+  },
+  overheat_red = {
+    hex = '#9B2226',
+    rgb = 'rgb(155, 34, 38)',
+    hsl = 'hsl(358, 64%, 37%)'
+  },
+  zero_gravity = {
+    hex = '#E0FBFC',
+    rgb = 'rgb(224, 251, 252)',
+    hsl = 'hsl(182, 74%, 93%)'
+  },
+  cryptid_gold = {
+    hex = '#C77DFF',
+    rgb = 'rgb(199, 125, 255)',
+    hsl = 'hsl(274, 100%, 75%)'
+  },
+  titanium_pulse = {
+    hex = '#778DA9',
+    rgb = 'rgb(119, 141, 169)',
+    hsl = 'hsl(214, 22%, 56%)'
+  },
+  bio_lumine = {
+    hex = '#38B000',
+    rgb = 'rgb(56, 176, 0)',
+    hsl = 'hsl(101, 100%, 35%)'
+  },
+  circuit_path = {
+    hex = '#2EC4B6',
+    rgb = 'rgb(46, 196, 182)',
+    hsl = 'hsl(174, 62%, 47%)'
+  },
+  stellar_dust = {
+    hex = '#FFD166',
+    rgb = 'rgb(255, 209, 102)',
+    hsl = 'hsl(42, 100%, 70%)'
+  },
+  mainframe_alert = {
+    hex = '#EF233C',
+    rgb = 'rgb(239, 35, 60)',
+    hsl = 'hsl(353, 86%, 54%)'
+  },
+  quantum_flux = {
+    hex = '#4CC9F0',
+    rgb = 'rgb(76, 201, 240)',
+    hsl = 'hsl(194, 86%, 62%)'
+  },
+  carbon_fiber = {
+    hex = '#161A1D',
+    rgb = 'rgb(22, 26, 29)',
+    hsl = 'hsl(206, 14%, 10%)'
+  },
+  obsidian_glass = {
+    hex = '#343A40',
+    rgb = 'rgb(52, 58, 64)',
+    hsl = 'hsl(210, 10%, 23%)'
+  },
+  neon_sulfur = {
+    hex = '#CCFF33',
+    rgb = 'rgb(204, 255, 51)',
+    hsl = 'hsl(75, 100%, 60%)'
+  },
+  acid_leak = {
+    hex = '#DFFF00',
+    rgb = 'rgb(223, 255, 0)',
+    hsl = 'hsl(68, 100%, 50%)'
+  },
+  solar_flare = {
+    hex = '#FB5607',
+    rgb = 'rgb(251, 86, 7)',
+    hsl = 'hsl(19, 97%, 51%)'
+  },
+  midnight_protocol = {
+    hex = '#14213D',
+    rgb = 'rgb(20, 33, 61)',
+    hsl = 'hsl(221, 51%, 16%)'
+  },
+  data_stream = {
+    hex = '#4361EE',
+    rgb = 'rgb(67, 97, 238)',
+    hsl = 'hsl(230, 83%, 60%)'
+  },
+  silicon_valley = {
+    hex = '#90E0EF',
+    rgb = 'rgb(144, 224, 239)',
+    hsl = 'hsl(190, 73%, 75%)'
+  },
+  nanotech_blue = {
+    hex = '#0096C7',
+    rgb = 'rgb(0, 150, 199)',
+    hsl = 'hsl(195, 100%, 39%)'
+  },
+  synthetic_leaf = {
+    hex = '#70E000',
+    rgb = 'rgb(112, 224, 0)',
+    hsl = 'hsl(90, 100%, 44%)'
+  },
+  warp_speed = {
+    hex = '#480CA8',
+    rgb = 'rgb(72, 12, 168)',
+    hsl = 'hsl(263, 87%, 35%)'
+  },
+  deep_buffer = {
+    hex = '#240046',
+    rgb = 'rgb(36, 0, 70)',
+    hsl = 'hsl(271, 100%, 14%)'
+  },
+  event_horizon = {
+    hex = '#000000',
+    rgb = 'rgb(0, 0, 0)',
+    hsl = 'hsl(0, 0%, 0%)'
+  }
+}
