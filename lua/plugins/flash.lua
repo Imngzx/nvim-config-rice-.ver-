6ff5669efffce28f8851932736ea29fba26ef387
Imngzx <117638365+Imngzx@users.noreply.github.com>
2026-04-11 13:36:28 +0800

    refactor(keymap): using enter instead of s for flash

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -4,8 +4,8 @@ lazy.load({
   plugin = 'https://github.com/folke/flash.nvim',
   event = { 'BufReadPost', 'BufNewFile' },
   keys = {
-    { { 'n', 'x', 'o' }, 's', function() require('flash').jump() end, { desc = 'Flash' } },
-    { { 'n', 'x', 'o' }, 'S', function() require('flash').treesitter() end, { desc = 'Flash treesitter' } },
+    { { 'n', 'x', 'o' }, '<CR>', function() require('flash').jump() end, { desc = 'Flash' } },
+    { { 'n', 'x', 'o' }, '<S-CR>', function() require('flash').treesitter() end, { desc = 'Flash treesitter' } },
     { 'o', 'r', function() require('flash').remote() end, { desc = 'Remote flash' } },
     { { 'o', 'x' }, 'R', function() require('flash').treesitter_search() end, { desc = 'Treesitter search' } },
     { { 'c' }, '<c-s>', function() require('flash').toggle() end, { desc = 'Toggle flash search' } }
