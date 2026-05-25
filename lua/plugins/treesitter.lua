e39719817b6747221aecb098698f2920719b9aa4
Imngzx <117638365+Imngzx@users.noreply.github.com>
2026-05-25 12:46:10 +0800

    perf(treesitter): async treesitter installation & fix lazy updater build hook

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,35 +1,48 @@
 local lazy = require('libs.lazy')
 
+local parsers = {
+  'c', 'cpp', 'python', 'lua', 'vim', 'vimdoc', 'markdown', 'markdown_inline',
+  'bash', 'json', 'yaml', 'toml', 'rust', 'zig', 'javascript', 'typescript', 'vue',
+  'latex', 'html', 'regex', 'css', 'gitcommit', 'fish', 'kdl',
+}
+
+local function setup_compiler()
+  if require('libs.utils').is_windows() then
+    vim.env.CC = 'gcc'
+    vim.env.CXX = 'g++'
+  end
+end
+
 lazy.load({
   plugin = { { src = 'https://github.com/nvim-treesitter/nvim-treesitter', version = 'main' } },
+
   build = function()
-    vim.cmd('TSUpdate')
+    setup_compiler()
+    vim.schedule(function()
+      local ok, ts = pcall(require, 'nvim-treesitter')
+      if ok and ts.update then
+        ts.update()()
+      else
+        pcall(vim.cmd, 'TSUpdate')
+      end
+    end)
   end,
+
   event = { 'BufReadPre', 'BufNewFile' },
 
   setup = function()
-    if require('libs.utils').is_windows() then
-      vim.env.CC = 'gcc'
-      vim.env.CXX = 'g++'
-    end
-
+    setup_compiler()
     local ts = require('nvim-treesitter')
-    local parsers = {
-      'c', 'cpp', 'python', 'lua', 'vim', 'vimdoc', 'markdown', 'markdown_inline',
-      'bash', 'json', 'yaml', 'toml', 'rust', 'zig', 'javascript', 'typescript', 'vue',
-      'latex', 'html', 'regex', 'css', 'gitcommit', 'fish', 'kdl',
-    }
 
-    ts.install(parsers, { summary = false })
+    vim.schedule(function()
+      ts.install(parsers, { summary = false })
+    end)
 
     vim.api.nvim_create_autocmd('FileType', {
       group = vim.api.nvim_create_augroup('TreesitterAttach', { clear = true }),
       callback = function(args)
         if vim.bo[args.buf].buftype ~= '' then return end
-
-        if vim.b[args.buf].snacks_bigfile then
-          return
-        end
+        if vim.b[args.buf].snacks_bigfile then return end
 
         local lang = vim.treesitter.language.get_lang(args.match)
         if lang then
