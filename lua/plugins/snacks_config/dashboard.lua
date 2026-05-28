ca7d9b8daf162b9f6d9f9214143398a9a13e8570
Imngzx <117638365+Imngzx@users.noreply.github.com>
2026-05-28 15:49:41 +0800

    fix(code): changed to resonance-ui

--- lua/plugins/snacks_config/dashboard.lua
+++ lua/plugins/snacks_config/dashboard.lua
@@ -9,7 +9,7 @@ return {
       { icon = ' ', key = 'c', desc = 'Config', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
       -- { icon = ' ', key = 's', desc = 'Restore Session', action = ':RestoreSession' },
       { icon = ' ', key = 's', desc = 'Restore Session', action = ":lua require('custom.session').load(false)" },
-      { icon = '󰒲 ', key = 'l', desc = 'DIY Lazy', action = ":lua require('custom.lazy-ui').open()" },
+      { icon = '󱑽 ', key = 'l', desc = 'Resonance', action = ":lua require('resonance').open_ui()" },
       { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
     },
   },
