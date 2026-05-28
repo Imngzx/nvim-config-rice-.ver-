a009902723c14baf8dd668a571f45f0fc17726c2
Imngzx <117638365+Imngzx@users.noreply.github.com>
2026-05-28 17:15:41 +0800

    refactor(catppuccin): made catppuccin handled by resonance

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,76 +1,50 @@
--- [Theme & Bug Fix]
+local resonance = require('resonance')
 local utils = require('libs.utils')
 
 local path_sep = utils.is_windows() and '\\' or '/'
 
--- nvim/lua/custom/catppuccin.lua
-vim.pack.add({ 'https://github.com/catppuccin/nvim' })
-require('catppuccin').setup({
-
-  -- NOTE: remember to :CatppuccinCompile first before long term of usage
-  -- Because it can reduce the coloring time
-  compile_path = vim.fn.stdpath('cache') .. path_sep .. 'catppuccin',
-
-  -- HACK: mocha, frappe, macchiato, latte
-  flavour = 'mocha',
-  transparent_background = false, -- 如果你想全局透明可以改成 true
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false
-  },
-  integrations = {
-    blink_cmp = { enabled = true, style = 'bordered' },
-    markdown = true,
-    mason = true,
-    render_markdown = true,
-    snacks = true,
-    which_key = true,
-    mini = true,
-    treesitter_context = true,
-    dropbar = { enabled = true, color_mode = false },
-    flash = true,
-    dap = true,
-  },
-  custom_highlights = function(colors)
-    return {
-      -- === 现有：清空 Markdown 语法的背景色 ===
-      -- ['@markup.heading.1.markdown'] = { bg = 'NONE' },
-      -- ['@markup.heading.2.markdown'] = { bg = 'NONE' },
-      -- ['@markup.heading.3.markdown'] = { bg = 'NONE' },
-      -- ['@markup.heading.4.markdown'] = { bg = 'NONE' },
-      -- ['@markup.heading.5.markdown'] = { bg = 'NONE' },
-      -- ['@markup.heading.6.markdown'] = { bg = 'NONE' },
-      -- ['@markup.heading.markdown'] = { bg = 'NONE' },
-      -- ['markdownH1'] = { bg = 'NONE' },
-      -- ['markdownH2'] = { bg = 'NONE' },
-      -- ['markdownH3'] = { bg = 'NONE' },
-      -- ['markdownH4'] = { bg = 'NONE' },
-      -- ['markdownH5'] = { bg = 'NONE' },
-      -- ['markdownH6'] = { bg = 'NONE' },
-
-      -- === 👇 新增：强制清空所有浮动窗口和边框的背景色 ===
-      --line number config
-      CursorLineNr = { bold = true },
-      LineNr = { fg = colors.surface1 },
-
-      -- 原生 LSP 和通用浮动窗口边框
-      NormalFloat = { bg = 'NONE' },
-      FloatBorder = { bg = 'NONE', fg = colors.pink }, -- fg可以换成你喜欢的边框颜色
-      FloatTitle = { bg = 'NONE' },
-
-      -- Blink.cmp 菜单边框 (图1的问题所在)
-      BlinkCmpMenuBorder = { bg = 'NONE', fg = colors.pink },
-      BlinkCmpDocBorder = { bg = 'NONE', fg = colors.blue },
-      BlinkCmpSignatureHelpBorder = { bg = 'NONE', fg = colors.blue },
-
-      WinBar = { bg = 'NONE' },
-      WinBarNC = { bg = 'NONE' },
-      DropBarMenuNormalFloat = { bg = 'NONE' },
-      DropBarMenuBorder = { bg = 'NONE', fg = colors.surface1 },
-
-
-      WhichKeyFloat = { bg = 'NONE' },
-    }
+resonance.load({
+  plugin = 'https://github.com/catppuccin/nvim',
+  event = { 'User', pattern = 'ForceLoadCatppuccin' },
+  setup = function()
+    require('catppuccin').setup({
+      compile_path = vim.fn.stdpath('cache') .. path_sep .. 'catppuccin',
+      flavour = 'mocha',
+      transparent_background = false,
+      highlight = { enable = true, additional_vim_regex_highlighting = false },
+      integrations = {
+        blink_cmp = { enabled = true, style = 'bordered' },
+        markdown = true,
+        mason = true,
+        render_markdown = true,
+        snacks = true,
+        which_key = true,
+        mini = true,
+        treesitter_context = true,
+        dropbar = { enabled = true, color_mode = false },
+        flash = true,
+        dap = true,
+      },
+      custom_highlights = function(colors)
+        return {
+          CursorLineNr = { bold = true },
+          LineNr = { fg = colors.surface1 },
+          NormalFloat = { bg = 'NONE' },
+          FloatBorder = { bg = 'NONE', fg = colors.pink },
+          FloatTitle = { bg = 'NONE' },
+          BlinkCmpMenuBorder = { bg = 'NONE', fg = colors.pink },
+          BlinkCmpDocBorder = { bg = 'NONE', fg = colors.blue },
+          BlinkCmpSignatureHelpBorder = { bg = 'NONE', fg = colors.blue },
+          WinBar = { bg = 'NONE' },
+          WinBarNC = { bg = 'NONE' },
+          DropBarMenuNormalFloat = { bg = 'NONE' },
+          DropBarMenuBorder = { bg = 'NONE', fg = colors.surface1 },
+          WhichKeyFloat = { bg = 'NONE' },
+        }
+      end
+    })
+    vim.cmd.colorscheme('catppuccin')
   end
 })
-vim.cmd.colorscheme('catppuccin')
+
+vim.api.nvim_exec_autocmds('User', { pattern = 'ForceLoadCatppuccin' })
