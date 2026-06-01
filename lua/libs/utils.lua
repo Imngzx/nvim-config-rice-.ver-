e1820ffa517c25d96f96e5a9971159d59b547b90
Imngzx <117638365+Imngzx@users.noreply.github.com>
2026-06-01 15:06:52 +0800

    fix(code): fixed logic

--- lua/libs/utils.lua
+++ lua/libs/utils.lua
@@ -7,12 +7,8 @@ end
 --- Check if the current nvim version is compatible with the allowed version
 --- @param version string
 --- @return boolean
-function M.is_compatible_version(version)
-  -- Old method
-  -- return vim.fn.has(string.format('nvim-%s', expected_version)) == 0
-  -- New method
-  -- https://neovim.io/doc/user/lua/#vim.version.le()
-  return vim.version.le(vim.version(), version)
+function M.is_compatible_version(min_version)
+  return vim.version.le(min_version, vim.version())
 end
 
 return M
