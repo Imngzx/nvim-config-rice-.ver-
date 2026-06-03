d6d485ccf758ce47717ebab4452ca2554d0e0681
Imngzx <117638365+Imngzx@users.noreply.github.com>
2026-06-03 08:20:02 +0800

    perf(code): optimized keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,167 +1,121 @@
+local map = vim.keymap.set
+local cmd = vim.cmd
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- [Basic]
--- Quit
-vim.keymap.set('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit all' })
-vim.keymap.set('n', '<leader>qr', '<cmd>restart<cr>', { desc = 'Restart' })
-
--- Save
-vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', { desc = 'Save file' })
-vim.keymap.set('n', '<leader>wq', '<cmd>wq<cr>', { desc = 'Save and quit' })
+-- Quit & Save
+map('n', '<leader>qq', '<cmd>qa<cr>', { desc = 'Quit all' })
+map('n', '<leader>qr', '<cmd>restart<cr>', { desc = 'Restart' })
+map('n', '<leader>w', '<cmd>w<cr>', { desc = 'Save file' })
+map('n', '<leader>wq', '<cmd>wq<cr>', { desc = 'Save and quit' })
 
 -- [View]
-vim.keymap.set('n', '<leader>us', '<cmd>setlocal spell! spell?<cr>', { desc = 'Toggle spelling' })
-vim.keymap.set('n', '<leader>uw', '<cmd>setlocal wrap! wrap?<cr>', { desc = 'Toggle wrap' })
-vim.keymap.set('n', '<leader>ub', '<cmd>lua vim.o.bg = vim.o.bg == "dark" and "light" or "dark"<cr>',
-  { desc = 'Toggle background' }
-)
+map('n', '<leader>us', '<cmd>setlocal spell! spell?<cr>', { desc = 'Toggle spelling' })
+map('n', '<leader>uw', '<cmd>setlocal wrap! wrap?<cr>', { desc = 'Toggle wrap' })
+map('n', '<leader>ub', '<cmd>lua vim.o.bg = vim.o.bg == "dark" and "light" or "dark"<cr>',
+  { desc = 'Toggle background' })
 
 -- [Edit]
 -- Indent
-vim.keymap.set({ 'n', 'v' }, 'j', 'gj')
-vim.keymap.set({ 'n', 'v' }, 'k', 'gk')
+map({ 'n', 'v' }, 'j', 'gj')
+map({ 'n', 'v' }, 'k', 'gk')
+map('x', '<', '<gv')
+map('x', '>', '>gv')
 
-vim.keymap.set('x', '<', '<gv')
-vim.keymap.set('x', '>', '>gv')
 -- Comment
-vim.keymap.set('n', 'gco', 'o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>', { desc = 'Add comment below' })
-vim.keymap.set('n', 'gcO', 'O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>', { desc = 'Add comment above' })
+map('n', 'gco', 'o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>', { desc = 'Add comment below' })
+map('n', 'gcO', 'O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>', { desc = 'Add comment above' })
+
 -- Move lines
-vim.keymap.set('n', '<a-k>', "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = 'Move up' })
-vim.keymap.set('n', '<a-j>', "<cmd>execute 'move .+' . v:count1<cr>==", { desc = 'Move down' })
-vim.keymap.set('i', '<a-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move up' })
-vim.keymap.set('i', '<a-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move down' })
-vim.keymap.set('v', '<a-k>', ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv",
-  { desc = 'Move up' })
-vim.keymap.set('v', '<a-j>', ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv",
-  { desc = 'Move down' })
+map('n', '<a-k>', "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = 'Move up' })
+map('n', '<a-j>', "<cmd>execute 'move .+' . v:count1<cr>==", { desc = 'Move down' })
+map('i', '<a-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move up' })
+map('i', '<a-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move down' })
+map('v', '<a-k>', ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = 'Move up' })
+map('v', '<a-j>', ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = 'Move down' })
+
 -- Spelling
-vim.keymap.set('n', '<leader>cs', 'z=', { desc = 'Spelling suggestions' })
+map('n', '<leader>cs', 'z=', { desc = 'Spelling suggestions' })
 
 -- [Buffer]
-vim.keymap.set('n', '<s-h>', '<cmd>bprevious<cr>', { desc = 'Prev buffer' })
-vim.keymap.set('n', '<s-l>', '<cmd>bnext<cr>', { desc = 'Next buffer' })
--- vim.keymap.set('n', '<leader>bd', function()
---   local cur = vim.api.nvim_get_current_buf()
---   local alt = vim.fn.bufnr('#')
---   if alt > 0 and vim.api.nvim_buf_is_loaded(alt) then
---     vim.cmd('buffer #')
---   else
---     vim.cmd('bnext')
---   end
---   vim.cmd('bdelete ' .. cur)
--- end, { desc = 'Delete buffer' })
--- vim.keymap.set('n', '<leader>bo', function()
---   local current = vim.api.nvim_get_current_buf()
---   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
---     if vim.api.nvim_buf_is_loaded(buf) and buf ~= current then vim.cmd('bdelete ' .. buf) end
---   end
--- end, { desc = 'Delete Other Buffers' })
-vim.keymap.set('n', '<leader>bn', '<cmd>enew<cr>', { desc = 'New file' }) -- new file
+map('n', '<s-h>', '<cmd>bprevious<cr>', { desc = 'Prev buffer' })
+map('n', '<s-l>', '<cmd>bnext<cr>', { desc = 'Next buffer' })
+map('n', '<leader>bn', '<cmd>enew<cr>', { desc = 'New file' })
 
 -- [Window]
-vim.keymap.set('n', '<leader>pd', '<c-w>c', { desc = 'Delete window', remap = true })
--- Split windows
-vim.keymap.set('n', '<leader>ps', '<c-w>s', { desc = 'Split window below', remap = true })
-vim.keymap.set('n', '<leader>pv', '<c-w>v', { desc = 'Split window right', remap = true })
+map('n', '<leader>pd', '<c-w>c', { desc = 'Delete window', remap = true })
+map('n', '<leader>ps', '<c-w>s', { desc = 'Split window below', remap = true })
+map('n', '<leader>pv', '<c-w>v', { desc = 'Split window right', remap = true })
+
 -- Move between windows
-vim.keymap.set('n', '<c-h>', '<c-w>h', { desc = 'Move to left window' })
-vim.keymap.set('n', '<bs>', '<c-w>h', { desc = 'Move to left window' }) -- Fix <c-h> used as <bs> in some terminal
-vim.keymap.set('n', '<c-j>', '<c-w>j', { desc = 'Move to below window' })
-vim.keymap.set('n', '<c-k>', '<c-w>k', { desc = 'Move to above window' })
-vim.keymap.set('n', '<c-l>', '<c-w>l', { desc = 'Move to right window' })
+map('n', '<c-h>', '<c-w>h', { desc = 'Move to left window' })
+map('n', '<bs>', '<c-w>h', { desc = 'Move to left window' }) -- Fix <bs> issue
+map('n', '<c-j>', '<c-w>j', { desc = 'Move to below window' })
+map('n', '<c-k>', '<c-w>k', { desc = 'Move to above window' })
+map('n', '<c-l>', '<c-w>l', { desc = 'Move to right window' })
+
 -- Resize splits
-vim.keymap.set('n', '<c-left>', function()
-  vim.cmd('vertical resize -' .. vim.v.count1)
-end, { desc = 'Decrease window width' })
+map('n', '<c-left>', function() cmd('vertical resize -' .. vim.v.count1) end,
+  { desc = 'Decrease window width' })
+map('n', '<c-down>', function() cmd('resize -' .. vim.v.count1) end,
+  { desc = 'Decrease window height' })
+map('n', '<c-up>', function() cmd('resize +' .. vim.v.count1) end,
+  { desc = 'Increase window height' })
+map('n', '<c-right>', function() cmd('vertical resize +' .. vim.v.count1) end,
+  { desc = 'Increase window width' })
 
-vim.keymap.set('n', '<c-down>', function()
-  vim.cmd('resize -' .. vim.v.count1)
-end, { desc = 'Decrease window height' })
-
-vim.keymap.set('n', '<c-up>', function()
-  vim.cmd('resize +' .. vim.v.count1)
-end, { desc = 'Increase window height' })
-
-vim.keymap.set('n', '<c-right>', function()
-  vim.cmd('vertical resize +' .. vim.v.count1)
-end, { desc = 'Increase window width' })
-
---[Functions]
--- Terminal
+-- [Functions]
 local utils = require('libs.utils')
 
-vim.keymap.set('n', '<leader>pt', function()
+-- Terminal
+map('n', '<leader>pt', function()
   local shell = ''
+  local fn = vim.fn
 
   if utils.is_windows() then
-    if vim.fn.executable('pwsh') == 1 then
+    if fn.executable('pwsh') == 1 then
       shell = 'pwsh'
-    elseif vim.fn.executable('powershell') == 1 then
+    elseif fn.executable('powershell') == 1 then
       shell = 'powershell'
     else
       shell = 'cmd'
     end
   else
-    if vim.fn.executable('fish') == 1 then
+    if fn.executable('fish') == 1 then
       shell = 'fish'
-    elseif vim.fn.executable('zsh') == 1 then
+    elseif fn.executable('zsh') == 1 then
       shell = 'zsh'
     else
       shell = vim.env.SHELL or 'bash'
     end
   end
 
-  vim.cmd('term ' .. shell)
-  -- vim.cmd('split | term ' .. shell)
-  -- vim.cmd('vsplit | term ' .. shell)
-
-  vim.cmd('startinsert')
+  cmd('term ' .. shell)
+  cmd('startinsert')
 end, { desc = 'Open Smart Terminal' })
 
--- Search
--- Better n/N behavior https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
-vim.keymap.set('n', 'n', "'Nn'[v:searchforward].'zv'", { expr = true, desc = 'Next search result' })
-vim.keymap.set('x', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next search result' })
-vim.keymap.set('o', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next search result' })
-vim.keymap.set('n', 'N', "'nN'[v:searchforward].'zv'", { expr = true, desc = 'Prev search result' })
-vim.keymap.set('x', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev search result' })
-vim.keymap.set('o', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev search result' })
+-- Search (Better n/N behavior)
+map('n', 'n', "'Nn'[v:searchforward].'zv'", { expr = true, desc = 'Next search result' })
+map('x', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next search result' })
+map('o', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next search result' })
+map('n', 'N', "'nN'[v:searchforward].'zv'", { expr = true, desc = 'Prev search result' })
+map('x', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev search result' })
+map('o', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev search result' })
+
 -- Clear search and stop snippet on escape
-vim.keymap.set({ 'i', 'n', 's' }, '<esc>',
-  function()
-    vim.cmd('noh')
-    return '<esc>'
-  end,
-  { expr = true, desc = 'Escape and clear hlsearch' }
-)
+map({ 'i', 'n', 's' }, '<esc>', function()
+  cmd('noh')
+  return '<esc>'
+end, { expr = true, desc = 'Escape and clear hlsearch' })
 
--- Yazi integragtion
-vim.keymap.set('n', '<leader>fy', function()
-  require('custom.yazi').open()
-end, { desc = 'Find via Yazi (File Manager)' })
+-- Yazi integration
+map('n', '<leader>fy', function() require('custom.yazi').open() end, { desc = 'Find via Yazi' })
 
--- Package
-vim.keymap.set('n', '<leader>pu', function() vim.pack.update() end, { desc = 'Update plugins' })
+-- Package update
+map('n', '<leader>pu', function() vim.pack.update() end, { desc = 'Update plugins' })
 
--- interesting feature...
 require('config.color_picker')
 
--- [Others]
--- -- location list
--- vim.keymap.set("n", "<leader>xl", function()
---   local success, err = pcall(vim.fn.getloclist(0, { winid = 0 }).winid ~= 0 and vim.cmd.lclose or vim.cmd.lopen)
---   if not success and err then
---     vim.notify(err, vim.log.levels.ERROR)
---   end
--- end, { desc = "Location List" })
--- -- quickfix list
--- vim.keymap.set("n", "<leader>xq", function()
---   local success, err = pcall(vim.fn.getqflist({ winid = 0 }).winid ~= 0 and vim.cmd.cclose or vim.cmd.copen)
---   if not success and err then
---     vim.notify(err, vim.log.levels.ERROR)
---   end
--- end, { desc = "Quickfix List" })
-
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
