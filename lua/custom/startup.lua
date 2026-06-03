409004052685da42e3c0bfb87e6450af995e724a
Imngzx <117638365+Imngzx@users.noreply.github.com>
2026-06-03 22:12:22 +0800

    chore(nvim): updated

--- lua/custom/startup.lua
+++ lua/custom/startup.lua
@@ -30,7 +30,7 @@ if vim.env.PROF then
   })
 end
 
-if not require('libs.utils').is_compatible_version('0x7fb2e35d2280') then
+if not require('libs.utils').is_compatible_version('0x7feb09cc5e50') then
   vim.notify('Need Neovim 0.13!', vim.log.levels.ERROR)
   return
 end
