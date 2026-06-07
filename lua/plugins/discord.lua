e370fa5e61b3819c93fe0335a11769b79fb5760d
Imngzx <117638365+Imngzx@users.noreply.github.com>
2026-06-07 20:10:37 +0800

    feat(build): added for cord.nvim

--- lua/plugins/discord.lua
+++ lua/plugins/discord.lua
@@ -3,6 +3,10 @@ local resonance = require('resonance')
 resonance.load({
   'https://github.com/vyfor/cord.nvim',
 
+  build = function()
+    vim.cmd('Cord update build')
+  end,
+
   event = { 'User', pattern = 'VeryLazy' },
 
   config = function()
