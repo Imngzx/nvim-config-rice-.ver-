e3da4dd8475d5cdf33a17262ed7dd2e6a5ab6590
Imngzx <117638365+Imngzx@users.noreply.github.com>
2026-06-09 08:53:35 +0800

    feat(atone): configured abit

--- lua/plugins/atone.lua
+++ lua/plugins/atone.lua
@@ -2,25 +2,30 @@ local resonance = require('resonance')
 
 resonance.load({
   'https://github.com/XXiaoA/atone.nvim',
-  -- 只有在使用命令或快捷键时才会加载这个插件
   cmd = { 'Atone' },
   keys = {
     { 'n', '<leader>uu', '<cmd>Atone toggle<cr>', { desc = 'Toggle Undo Tree (Atone)' } },
   },
   config = function()
     require('atone').setup({
-      -- 下面都是默认可选配置，可根据喜好调整
       layout = {
         direction = 'left',
-        width = 0.25, -- 如果小于 1 则视为屏幕宽度的百分比
+        width = 0.25,
       },
       diff_cur_node = {
         enabled = true,
         split_percent = 0.3,
       },
       marks = {
-        persist = true, -- 保存书签，哪怕重启 Nvim 也不丢
+        persist = true,
       },
+      ui = {
+        border = 'rounded',
+        compact = true,
+        node_label = {
+          custom = true,
+        },
+      }
     })
   end
 })
