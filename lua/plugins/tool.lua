44b709925934040f88460057be513396ae164839
Imngzx <117638365+Imngzx@users.noreply.github.com>
2026-07-01 20:34:48 +0800

    fix(author): changed author

--- lua/plugins/tool.lua
+++ lua/plugins/tool.lua
@@ -119,7 +119,7 @@ resonance.load({
   },
 
   {
-    'https://github.com/NeogitOrg/neogit',
+    'https://github.com/aurora0x27/neogit',
     dependencies = 'https://github.com/nvim-lua/plenary.nvim',
 
     cmd = 'Neogit',
