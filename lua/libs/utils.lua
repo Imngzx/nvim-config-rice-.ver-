dc79c40b01b05850570a291abcd86681484d8cf6
Imngzx <117638365+Imngzx@users.noreply.github.com>
2026-07-16 20:54:16 +0800

    refactor(version-check): rewrite

--- lua/libs/utils.lua
+++ lua/libs/utils.lua
@@ -16,7 +16,7 @@ end
 ---@param version string
 ---@return boolean
 function M.is_compatible_version(min_version)
-  return vim.version.le(min_version, vim.version())
+  return vim.version() >= vim.version.parse(min_version)
 end
 
 return M
