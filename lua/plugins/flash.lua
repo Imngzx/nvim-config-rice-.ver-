8bc150677fc877c09f764f26b3462169fdd9b19e
Imngzx <117638365+Imngzx@users.noreply.github.com>
2026-07-18 16:46:16 +0800

    fix(plugin-author): compatibility issues between neovim nightly and flash.nvim

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,7 +1,7 @@
 local resonance = require('resonance')
 
 resonance.load({
-  'https://github.com/folke/flash.nvim',
+  'https://github.com/onion108/flash.nvim',
   keys = {
     { { 'n', 'x', 'o' }, '<CR>', function() require('flash').jump() end, { desc = 'Flash' } },
     { { 'n', 'x', 'o' }, '<S-CR>', function() require('flash').treesitter() end, { desc = 'Flash treesitter' } },
